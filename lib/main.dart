eefb23ae2f9315247fa87cb455bd30d05df28934
iftixorFSD <etgashboeviftixor@gmail.com>
2024-08-06 10:41:11 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:crypto_app/page/main_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CryptoApp());
+}
+class CryptoApp extends StatelessWidget {
+  const CryptoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true
+      ),
+      home: const MainPage(),
+    );
+  }
+}
---
+++ lib/page/main_page.dart
@@ -0,0 +1,54 @@
+import 'package:crypto_app/page/profile_page.dart';
+import 'package:crypto_app/page/trade_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = const [
+    HomePage(),
+    TradePage(),
+    ProfilePage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.rectangle_3_offgrid),
+
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.currency_exchange_rounded),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
